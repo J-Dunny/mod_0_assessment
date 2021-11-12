d74130575c9d26465af3085e56b43356fdd32790
Jake Dunafon <jlad795@gmail.com>
2021-11-12 13:42:41 -0800

    Initial Commit

---
+++ variables.js
@@ -0,0 +1,17 @@
+var animal = "Jaguar";
+
+var isCuddly = true;
+
+animal = "Leapord";
+
+var num8 = 8;
+
+var num3 = 3;
+
+var quotient = num3 / num8;
+
+var favoriteFilms = ["Saving Private Ryan", "Wolf of Wallstreet", "Fight Club"];
+
+favoriteFilms.shift();
+
+console.log(animal.length);
