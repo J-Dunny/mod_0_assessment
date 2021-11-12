f7906b3e22ae4ee2122cd465de5bda45ec6386cb
Jake Dunafon <jlad795@gmail.com>
2021-11-12 13:48:54 -0800

    Add laptop and house objects

---
+++ models/modelA.js
@@ -0,0 +1,11 @@
+var Laptop = {
+  brand: "Apple",
+  version: "Macbook Pro",
+  price: 2000,
+  numAvailable: 20,
+  isNew: true,
+  hasWebcam: true,
+  specs: ["grey", "Siri enabled", "500 Gb"],
+  dimensions: ["13 in W", "10 in L", "1.2 in H"]
+  
+}
---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var house = {
+  color: "White",
+  type: "Huge",
+  sqrFootage: 12,000,
+  numRooms: 15,
+  forSale: false,
+  haunted: true,
+  features: ["AC", "Ghosts", "Heat"],
+  extras: ["Pool", "Hot tub", "Basketball Court"]
+}
